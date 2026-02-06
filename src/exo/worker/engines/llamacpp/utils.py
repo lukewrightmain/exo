6ab03c444b1a78279e2e86b3a27c98963bed23f3
lukewrightmain <lukewrightmain@gmail.com>
2026-02-06 14:08:22 -0600

    Increase DISTRIBUTED_SERVER_STARTUP_TIMEOUT to 2400s (40 min)
    Loading was timing out at 1800s but workers reported READY just 5-14s
    after timeout. 2400s provides comfortable margin for 3-node WiFi.

--- src/exo/worker/engines/llamacpp/utils.py
+++ src/exo/worker/engines/llamacpp/utils.py
@@ -21,7 +21,7 @@ from exo.worker.engines.llamacpp.constants import (
 from exo.worker.runner.bootstrap import logger
 
 
-DISTRIBUTED_SERVER_STARTUP_TIMEOUT: Final[int] = 1800  # 30 minutes for multi-node WiFi tensor transfer
+DISTRIBUTED_SERVER_STARTUP_TIMEOUT: Final[int] = 2400  # 40 minutes for multi-node WiFi tensor transfer
 
 
 def is_android() -> bool:
